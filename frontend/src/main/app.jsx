e0a6a7b3e66f0c619f24471b66d45e791fa4c94f
higor21 <felype.cet.15@gmail.com>
2019-08-20 22:58:49 -0300

    starting to create tabs component

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import Container from "@material-ui/core/Container"
 
 import Header from './common/template/Header/header'
+import Tabs from './common/template/Tabs/Tabs'
 
 export default props => (
     <div>
         <Header />
-        <Container maxWidth="xl" fixed>
-        
+        <Container maxWidth="md" fixed>
+            <Tabs />
         </Container>
     </div>
 )
---
+++ frontend/src/main/common/template/Tabs/Tabs.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Tab from './Tab/Tab'
+
+export default props => {
+    return (
+        <div>
+            <Tab />
+            <Tab />
+            <Tab />
+        </div>
+    )
+}
